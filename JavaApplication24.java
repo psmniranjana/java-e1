19ff5a6885309099754c10de4e5f93a8b7fc9f91
Maleesha Niranjana <117069472+psmniranjana@users.noreply.github.com>
2023-03-30 14:56:40 +0530

    Add files via upload

---
+++ JavaApplication24.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication24;
+
+public class JavaApplication24 {
+
+     void LoadingMessage(){
+         System.out.println("Loading...");
+     }
+    public static void main(String[] args) {
+        JavaApplication24 loading=new JavaApplication24();
+       //create a Loading object with the same name
+       
+       
+       loading.LoadingMessage();
+    }
+    
+}
